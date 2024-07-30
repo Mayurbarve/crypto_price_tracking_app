87e42d9746ae4c3d415f63c24d5d5ac21b71dc81
Mayurbarve <mayurbarve04@gmail.com>
2024-07-30 18:34:41 +0530

    Updated

--- src/component/Footer/Footer.jsx
+++ src/component/Footer/Footer.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div className='text-center m-auto w-[80%] text-m'>
+    <div className='text-center mb-6 m-auto w-[80%] text-m'>
         Copyrigth @ 2024, CryptoCoin - All Right Reserved.
     </div>
   )
